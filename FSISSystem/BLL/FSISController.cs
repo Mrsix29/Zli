836a6163c100dc2bd107d81044c01537a719ce30
Zongyao Li <zli64@nait.ca>
2020-03-09 16:44:59 -0600

    March 9

---
+++ FSISSystem/BLL/FSISController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FSISSystem.ENTITIES;
+using FSISSystem.DAL;
+namespace FSISSystem.BLL
+{
+    public class GuardianController
+    {
+        public Guardian Guardians_FindByID(int GuardianID)
+        {
+            using (var context = new FSISContext())
+            {
+                return context.Guardians.Find(GuardianID);
+            }
+        }
+    }
+    public class PlayerController
+    {
+    }
+    public class TeamController
+    {
+    }
+}
